5ff8f4f90166cfe8e74dbeb81f76845fe6d6ed28
heyyakash <akashsharma2002@gmail.com>
2023-09-13 14:50:29 +0530

    created route for lawyers too !!

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,29 +1,34 @@
-const userTypes = ["user","lawyer","other"]
+const userTypes = ["user", "lawyer", "other"]
 const User = require("../modals/user.modal")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const Login = require("../modals/login.modal")
 const ErrorHandler = require("../utils/errorHandler")
 const catchAsyncError = require("../middleware/catchAsyncErrors");
+const Lawyer = require("../modals/lawyer.modal")
 
-exports.createUser = catchAsyncError(async (req,res,next) => {
-    const {userType} = req.body
-    if(!userType) return next(new ErrorHandler('Send UserType idiot',400));
+exports.createUser = catchAsyncError(async (req, res, next) => {
+    //validating request
+    const { userType } = req.body
+    if (!userType) return next(new ErrorHandler('Send UserType idiot', 400));
 
-    if(!userTypes.includes(userType)) return next(new ErrorHandler('Invalid User Type',400));
+    if (!userTypes.includes(userType)) return next(new ErrorHandler('Invalid User Type', 400));
 
-    if(userType==="user"){
-        const {firstName, lastName, email, gender, contact, city, state ,postalCode, aadharNumber, password} = req.body
-        if(!firstName || !lastName || !email || !gender || !contact || !city || !state || !postalCode ||!aadharNumber ||!password){
-            return next(new ErrorHandler("Parameter missing bruh!",400));
-        }
+    const { firstName, lastName, email, gender, contact, city, state, postalCode, aadharNumber, password } = req.body
+    if (!firstName || !lastName || !email || !gender || !contact || !city || !state || !postalCode || !aadharNumber || !password) {
+        return next(new ErrorHandler("Parameter missing bruh!", 400));
+    }
 
-        const existingUser = await Login.findOne({email})
+    //checking for existing user
+    const existingUser = await Login.findOne({ email })
+    if (existingUser) return next(new ErrorHandler("User Already Exists", 400));
 
-        if(existingUser) return next(new ErrorHandler("User Already Exists",400));
+    //hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(password, salt);
+    //adding data to db
+    if (userType === "user") {
         const user = await User.create(req.body)
         await Login.create({
             email,
@@ -31,7 +36,19 @@ exports.createUser = catchAsyncError(async (req,res,next) => {
             uid: user["_id"],
             userType
         })
-        return res.status(200).json({msg:"Wohhooo !! User Created ", success:true})
+        return res.status(200).json({ msg: "Wohhooo !! User Created ", success: true })
     }
-    return next(new ErrorHandler("Route under Construction ! ",500));
+    else if (userType === "lawyer") {
+        const {bio, courts, regId} = req.body
+        if(!bio || !courts || !regId) return next(new ErrorHandler("Missing Parameters for laywer ",400))
+        const user = await Lawyer.create(req.body)
+        await Login.create({
+            email,
+            password: hashedPass,
+            uid: user["_id"],
+            userType
+        })
+        return res.status(200).json({ msg: "Wohhooo !! Lawyer Created ", success: true })
+    }
+    return next(new ErrorHandler("Route under Construction ! ", 500));
 })
---
+++ Backend/modals/lawyer.modal.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require("mongoose")
+
+const lawyer = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: Number,
+        required: true,
+        minlength: [10, "Invalid Mobile number"],
+        maxLength: [10, "Invalid Mobile number"]
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    postalCode: {
+        type: String,
+        required: true
+    },
+    aadharNumber: {
+        type: String,
+        required: true,
+        minlength: [12, "Invalid Aadhaar Number"],
+        maxlength: [12, "Invalid Aadhaar Number"],
+    },
+    regId: {
+        type: String,
+        required: true
+    },
+    courts :[
+        {
+            type: String,
+            required: true
+        }
+    ],
+    bio:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports = model("lawyers", lawyer)
